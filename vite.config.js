0c49332c82da53de83a26c4c0a93248d29a64e54
Prasanth-S-K <prasanthk12051999@gmail.com>
2025-10-08 21:24:56 +0530

    little update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// If deploying to a subfolder, set base:
+// base: '/My_Portfolio/'
+// For Vercel root domain, you can leave it as '/'
 export default defineConfig({
-  base: "/My_Portfolio/", // ✅ Add this line
   plugins: [react()],
+  base: "./", // important! ensures assets load correctly
 });
